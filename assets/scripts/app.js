58c9896273e71e386c7bcc12570632c9c084df70
hellwinkel <guirovedah@gmail.com>
2020-12-04 15:23:36 -0300

    Product module almost done

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,24 @@
+$(document).ready(function() {
+  $('.swiper-slide .item-title').each(function() {
+    $(this).text(limitProductName($(this).text().trim()))
+  })
+
+  function limitProductName(productName) {
+    if(productName.length > 58) {
+      return productName.substring(0, 55) + '...'
+    } else {
+      return productName
+    }
+  }
+})
+
+
 let swiper = new Swiper('.swiper-container', {
   centeredSlides: false,
   loop: false,
   slidesPerGroup: 1,
   slidesPerView: 1,
-  spaceBetween: 25,
+  spaceBetween: 20,
   simulateTouch: false,
   navigation: {
     nextEl: '.swiper-custom-button-next',
@@ -19,3 +34,29 @@ let swiper = new Swiper('.swiper-container', {
     2495: {slidesPerView: 8}
   }
 });
+
+function focusProduct(element) {
+  if(element) {
+    element.siblings().addClass('blur')
+  } else {
+    $('.swiper-slide').removeClass('blur')
+  }
+}
+
+$('.swiper-slide').on('focusin', function() {
+  focusProduct($(this))
+})
+
+$('.swiper-slide').on('focusout', function() {
+  focusProduct()
+})
+
+$('.swiper-slide').on('mouseover', function() {
+  focusProduct($(this))
+})
+
+$('.swiper-slide').on('mouseout', function() {
+  focusProduct()
+})
+
+
