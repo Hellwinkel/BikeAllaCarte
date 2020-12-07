e7f07a80da6139fd78e88ef28e52532b9cb54231
hellwinkel <guirovedah@gmail.com>
2020-12-07 11:20:26 -0300

    Module button now have a fantastic animation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
+  
+  // -------------- Apagar quando função PHP que restringe o tamanho do nome do produto estiver funcionando
   $('.swiper-slide .item-title').each(function() {
     $(this).text(limitProductName($(this).text().trim()))
   })
-
   function limitProductName(productName) {
     if(productName.length > 58) {
       return productName.substring(0, 55) + '...'
@@ -10,53 +11,84 @@ $(document).ready(function() {
       return productName
     }
   }
+  // -------------- 
 })
 
 
-let swiper = new Swiper('.swiper-container', {
-  centeredSlides: false,
-  loop: false,
-  slidesPerGroup: 1,
-  slidesPerView: 1,
-  spaceBetween: 20,
-  simulateTouch: false,
-  navigation: {
-    nextEl: '.swiper-custom-button-next',
-    prevEl: '.swiper-custom-button-prev'
-  },
-  breakpoints: {
-    750: {slidesPerView: 2},
-    1045: {slidesPerView: 3},
-    1335: {slidesPerView: 4},
-    1625: {slidesPerView: 5},
-    1915: {slidesPerView: 6},
-    2205: {slidesPerView: 7},
-    2495: {slidesPerView: 8}
-  }
-});
-
-function focusProduct(element) {
-  if(element) {
-    element.siblings().addClass('blur')
-  } else {
-    $('.swiper-slide').removeClass('blur')
-  }
+// Create swiper instance
+{
+  // Note: to work, each slider must have a unique ID defined with 'swiper-container' and as part of its navigation arrows
+  $('.swiper-container').each(function() {
+    let id = $(this).attr('id')
+  
+    new Swiper(`.swiper-container#${id}`, {
+      centeredSlides: false,
+      loop: false,
+      slidesPerGroup: 1,
+      slidesPerView: 1,
+      spaceBetween: 25,
+      simulateTouch: false,
+      navigation: {
+        nextEl: `#${id}-swiper-next`,
+        prevEl: `#${id}-swiper-prev`
+      },
+      breakpoints: {
+        750: {slidesPerView: 2},
+        1045: {slidesPerView: 3},
+        1335: {slidesPerView: 4},
+        1625: {slidesPerView: 5},
+        1915: {slidesPerView: 6},
+        2205: {slidesPerView: 7},
+        2495: {slidesPerView: 8}
+      }
+    }); 
+  })
 }
 
-$('.swiper-slide').on('focusin', function() {
-  focusProduct($(this))
-})
 
-$('.swiper-slide').on('focusout', function() {
-  focusProduct()
-})
+// Create hover effect on products
+{
+  function focusProduct(element) {
+    if(element) {
+      element.siblings().addClass('blur')
+    } else {
+      $('.swiper-slide').removeClass('blur')
+    }
+  }
+  
+  $('.swiper-slide').on('focusin', function() {
+    focusProduct($(this))
+  })
+  
+  $('.swiper-slide').on('focusout', function() {
+    focusProduct()
+  })
+  
+  $('.swiper-slide').on('mouseover', function() {
+    focusProduct($(this))
+  })
+  
+  $('.swiper-slide').on('mouseout', function() {
+    focusProduct()
+  })
+}
 
-$('.swiper-slide').on('mouseover', function() {
-  focusProduct($(this))
-})
 
-$('.swiper-slide').on('mouseout', function() {
-  focusProduct()
-})
-
-
+// Create hover effect on module button
+{
+  $('.module-link').on('mouseover', function() {
+    $(this).addClass('hover')
+  })
+  
+  $('.module-link').on('focusin', function() {
+    $(this).addClass('hover')
+  })
+  
+  $('.module-link').on('focusout', function() {
+    $(this).removeClass('hover')
+  })
+  
+  $('.module-link').on('mouseout', function() {
+    $(this).removeClass('hover')
+  })
+}
