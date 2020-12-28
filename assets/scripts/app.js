61c81a2ca55b3fea76a9373b2be68bd2b36cbd17
Hellwinkel <guirovedah@gmail.com>
2020-12-28 15:30:52 -0300

    God is among us. We have a miracle here! Breakpoint 1199px was fixed

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,6 @@ let overflowController = 0
 let scrollTop = 0
 
 $(document).ready(function () {
-
   // Fix VH in mobile devices
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
